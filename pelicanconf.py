3fa31bcb82ee2b964764b24bf35ed110bf15772a
Lisa K. Johnson <ljcohen@ucdavis.edu>
2018-12-05 16:38:20 -0800

    Update pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,7 +6,7 @@ import os
 AUTHOR = 'GIGA'
 SITENAME = 'GIGA - Global Invertebrate Genomics Alliance'
 SITEDESCRIPTION = 'Website for the Global Invertebrate Genomics Alliance'
-SITEURL = 'giga.github.io'
+SITEURL = 'GlobalInvertebrateGenomicsAlliance.github.io'
 
 # plugins
 PLUGIN_PATHS = ['./plugins']
